dc5f892e95d076279154c0df604fa08d1736da9f
Podlesnyy Andrey <podlesniy@gmail.com>
2014-11-12 12:05:15 +0300

    Gateway and subnet

--- Sketches/Servo/Servo.ino
+++ Sketches/Servo/Servo.ino
@@ -3,13 +3,15 @@
 #include <Servo.h>
 
 Servo myservo;
-byte arduinoMac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte arduinoIp[] = { 192, 168, 20, 177 };
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,20,177);
+IPAddress gateway(192,168,20,1);
+IPAddress subnet(255, 255, 255, 0);
 const int serverPort = 9600;
 EthernetServer server = EthernetServer(serverPort);
 
 void setup(){
-  Ethernet.begin(arduinoMac, arduinoIp);
+  Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
   const int controlledPin = 3;
   myservo.attach(controlledPin);
